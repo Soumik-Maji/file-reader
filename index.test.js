55ce4b378a6133df23af831703759c8e637f0edf
soumik <soumikmaji2000@gmail.com>
2026-01-18 22:01:30 +0530

    tested previous commit code & wrote package.json

---
+++ index.test.js
@@ -0,0 +1,9 @@
+import { filepathtest } from "./tests/test-scripts/filepathtest.js";
+import { inputelementtest } from "./tests/test-scripts/inputelementtest.js";
+import { rawstringtest } from "./tests/test-scripts/rawstringtest.js";
+import { textareatest } from "./tests/test-scripts/textareatest.js";
+
+await rawstringtest();
+await textareatest();
+await inputelementtest();
+await filepathtest();
